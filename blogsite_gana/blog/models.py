f09ad0f32f47e71d4f42b6b2c0100fd33564bceb
gana-95 <gana95@uw.edu>
2021-07-13 23:02:06 -0700

    Made changes on blog site files

--- blogsite_gana/blog/models.py
+++ blogsite_gana/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -24,6 +25,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True) # Time when the post is first created
     updated = models.DateTimeField(auto_now=True) # Last time when the post is updated
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft') # Shows whether the post is a draft or published
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
--- blogsite_gana/blog/views.py
+++ blogsite_gana/blog/views.py
@@ -4,6 +4,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -12,8 +14,12 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 # View to see all the posts on the blog.
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -27,7 +33,8 @@ def post_list(request):
     return render(request,
     'blog/post/list.html',
     {'page': page,
-    'posts': posts})
+    'posts': posts,
+    'tag': tag})
 
 # View to to see particular posts based on the details
 def post_detail(request, year, month, day, post):
@@ -52,12 +59,18 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
     return render(request,
     'blog/post/detail.html',
     {'post': post,
     'comments': comments,
     'new_comment': new_comment,
-    'comment_form': comment_form})
+    'comment_form': comment_form,
+    'similar_posts': similar_posts})
 
 # To share the posts to others
 def post_share(request, post_id):
