441171a73a17adf4e5357d979fcf4e26848ac210
gana-95 <gana95@uw.edu>
2021-07-13 20:58:42 -0700

    git add .

--- blogsite_gana/blog/admin.py
+++ blogsite_gana/blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Post
-@admin.register(Post)
+from .models import Post, Comment
 
 # Register your models here.
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
@@ -11,3 +11,9 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
--- blogsite_gana/blog/models.py
+++ blogsite_gana/blog/models.py
@@ -10,6 +10,7 @@ class PublishedManager(models.Manager):
                     self).get_queryset()\
                         .filter(status='published')
 
+# Database model for the Posts created
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -40,3 +41,15 @@ class Post(models.Model):
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        ordering = ('created',)
+    def __str__(self):
+        return f'Commen by {self.name} on {self.post}'
